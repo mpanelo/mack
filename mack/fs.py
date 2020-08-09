bde91f704dcca96b45cb3a206c0aa06715a9484a
Mauricio Panelo JR <17281354+mpanelo@users.noreply.github.com>
2020-08-09 01:12:21 -0700

    refactor main method to read enron emails in batches and create a monolithic index file

--- mack/fs.py
+++ mack/fs.py
@@ -5,16 +5,19 @@ from collections import namedtuple
 Document = namedtuple("Document", "id name text")
 
 
-def read(root):
+def batch_read(root, batch_size=100):
     documents = []
     counter = 0
 
-    for path, sub_dirs, files in os.walk(root):
-        for filename in files:
-            abs_path = os.path.join(path, filename)
-            with open(abs_path) as file:
-                text = file.read()
-                documents.append(Document(id=counter, name=abs_path, text=text))
-            counter += 1
+    for path, sub_dirs, file_names in os.walk(root):
+        for file_name in file_names:
+            abs_file_path = os.path.join(path, file_name)
 
-    return documents
+            with open(abs_file_path) as file:
+                documents.append(Document(id=counter, name=abs_file_path, text=file.read()))
+
+            if len(documents) > batch_size:
+                yield documents
+                documents = []
+
+            counter += 1
--- mack/main.py
+++ mack/main.py
@@ -1,18 +1,22 @@
 from mack import fs
 from mack import search
-
-documents = {}
+from mack import index_io
 
 
 def main():
-    index = search.DictionaryIndex()
-    inverted_index = search.InvertedIndex(index)
+    segment_generator = index_io.SegmentGenerator(root="inverted_index_segments")
+    writer = index_io.Writer(segment_generator)
 
-    for document in fs.read("enron"):
-        inverted_index.add(document)
-        documents[document.id] = document
+    inverted_index = search.DictionaryInvertedIndex()
 
-    inverted_index.save('test.index')
+    for documents in fs.batch_read("enron", 100):
+        for document in documents:
+            inverted_index.add(document)
+        writer.write(inverted_index)
+        inverted_index.clear()
+
+    merger = index_io.Merger(destination="merged_segments.index")
+    merger.merge(segment_generator.get_generated_segment_paths())
 
 
 if __name__ == "__main__":
