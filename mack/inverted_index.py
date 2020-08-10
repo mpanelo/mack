54babfa116a0e001df71d42738756ff6a0fa866e
Mauricio Panelo JR <17281354+mpanelo@users.noreply.github.com>
2020-08-09 21:44:53 -0700

    add Trie implementation of inverted index

--- mack/inverted_index.py
+++ mack/inverted_index.py
@@ -39,3 +39,58 @@ class DictionaryInvertedIndex:
     def clear(self):
         self._index.clear()
 
+
+class TrieNode:
+    ASCII_TABLE_SIZE = 127
+
+    def __init__(self, value=None, is_leaf=False):
+        self.value = value
+        self.is_leaf = is_leaf
+        self.children = [None for _ in range(self.ASCII_TABLE_SIZE)]
+
+
+class TrieInvertedIndex:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, term, record):
+        curr_node = self.root
+
+        for char in term:
+            char_index = ord(char)
+
+            if curr_node.children[char_index] is None:
+                next_node = TrieNode()
+                curr_node.children[char_index] = next_node
+            else:
+                next_node = curr_node.children[char_index]
+
+            curr_node = next_node
+
+        curr_node.is_leaf = True
+        curr_node.value = record
+
+    def prefix_search(self, prefix):
+        curr_node = self.root
+
+        for char in prefix:
+            char_index = ord(char)
+            curr_node = curr_node.children[char_index]
+
+            if curr_node is None:
+                return []
+
+        result = []
+        self._recursive_search(curr_node, list(prefix), result)
+        return result
+
+    def _recursive_search(self, root, chars, result):
+        if root.is_leaf:
+            result.append((''.join(chars), root.value))
+
+        for char_index, child in enumerate(root.children):
+            if child is not None:
+                chars.append(chr(char_index))
+                self._recursive_search(child, chars, result)
+                chars.pop()
